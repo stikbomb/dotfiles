e531b781748d89a152a656a1ae4832030ccbc916
stikbomb <a.molodid@gmail.com>
2026-03-07 12:58:24 +0300

    refactor: move nva fully into chezmoi, drop separate git repo

--- dot_config/nva/lua/plugins/codeium.lua
+++ dot_config/nva/lua/plugins/codeium.lua
@@ -1,4 +1,18 @@
 return {
-  "Exafunction/codeium.vim",
-  event = "BufEnter",
+  {
+    "Exafunction/codeium.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    build = ":Codeium Auth",
+    opts = {
+      enable_cmp_source = false,
+      virtual_text = {
+        enabled = true,
+        key_bindings = {
+          accept = "<Tab>",
+          next = "<M-]>",
+          prev = "<M-[>",
+        },
+      },
+    },
+  },
 }
